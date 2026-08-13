e7c5f363cdc28d24adc9e6f2eb8dacc8a918bf70
milligenclikmgd <milligenclikmgd@gmail.com>
2026-08-13 14:44:48 +0300

    Remove unused map reference import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import { 
   BrowserRouter as Router, Routes, Route, Link, useNavigate, useLocation 
